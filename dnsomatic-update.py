110646360eaff29fc8837fb7a20ed7ee95e53e73
Jason Costomiris <jcostom@jasons.org>
2022-04-17 23:13:03 -0400

    one of these times, I'll get this right...

--- dnsomatic-update.py
+++ dnsomatic-update.py
@@ -23,7 +23,7 @@ SITENAME = os.getenv('SITENAME', 'mysite')
 DEBUG = int(os.getenv('DEBUG', 0))
 
 IPCACHE = "/config/ip.cache.txt"
-VER = "3.3.3"
+VER = "3.3.4"
 USER_AGENT = f"dnsomatic-update.py/{VER}"
 
 # Setup logger
